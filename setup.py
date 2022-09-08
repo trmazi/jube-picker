55a2a3bbe9615417b5de6430ae66e773444faa54
Trenton Zimmer <66042448+trmazi@users.noreply.github.com>
2022-09-07 23:40:37 -0400

    just use autopy2exe lol

--- setup.py
+++
@@ -1,9 +0,0 @@
-import cx_Freeze
-
-executables = [cx_Freeze.Executable("./picker.py")]
-
-cx_Freeze.setup(
-    name="JubePicker",
-    options={"build_exe": {"packages":["pygame"]}},
-    executables = executables
-)
